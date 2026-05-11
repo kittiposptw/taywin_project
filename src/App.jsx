d71fb9b13815d1be4a89009d276439117ad666a6
Kittipos <kittipos.ptw@gmail.com>
2026-05-11 16:07:52 +0700

    fix: scroll to top on every page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import HomePage from './pages/HomePage'
 import ProductPage from './pages/ProductPage'
 import CheckoutPage from './pages/CheckoutPage'
 import HeritagePage from './pages/HeritagePage'
 import CollectionsPage from './pages/CollectionsPage'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
+  return null
+}
+
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/collections" element={<CollectionsPage />} />
