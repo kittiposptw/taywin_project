51ec325dfcf2dd04e94554d7a3e3078af8746990
Kittipos <kittipos.ptw@gmail.com>
2026-05-11 12:06:13 +0700

    feat: update product route to /product/:sku

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import ProductPage from './pages/ProductPage'
 import CheckoutPage from './pages/CheckoutPage'
+import HeritagePage from './pages/HeritagePage'
+import CollectionsPage from './pages/CollectionsPage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/product" element={<ProductPage />} />
+        <Route path="/collections" element={<CollectionsPage />} />
+        <Route path="/heritage" element={<HeritagePage />} />
+        <Route path="/product/:sku" element={<ProductPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
       </Routes>
     </BrowserRouter>
